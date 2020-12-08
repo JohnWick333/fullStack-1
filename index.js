b6e94ead4ff6c0fef65765d571d73496c8a2edce
Saumik Nondi <n.saumik333@gmail.com>
2020-12-09 01:15:39 +0600

    Oauth working

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const app = express();
-app.get('/',(req,res,next)=>{
-    res.send('Hi there!')
-})
+const mongoose = require('mongoose')
+const passport= require('passport')
+const cookieSession = require('cookie-session')
+require('./models/Users')
+require('./services/passport')
+const appRoutes = require('./routes/authRoutes')
+const keys = require('./config/keys')
 
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true,useUnifiedTopology: true})
+
+const app = express();
+app.use(cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys:[keys.cookieKey]
+}))
+
+app.use(passport.initialize());
+app.use(passport.session())
+
+appRoutes(app);
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server started on port`);
